faf850f20055703b8f3d19a526ceedaaaf396ad6
jang-199 <jw1010110@gmail.com>
2022-10-26 00:48:06 +0900

    Modified Receiver 패킷이 연속으로 전송되지 않는 문제 해결

--- Arduino/receiver/result/result.ino
+++ Arduino/receiver/result/result.ino
@@ -7,6 +7,7 @@ String tempForBat = "110";
 String bat;
 unsigned long currentTimeForBat, previousTimeForBat = 0;
 
+//wifi connect
 String url = "";
 char ssid[] = "U+NetCE64";    // your network SSID (name)
 char pass[] = "DD45019067";         // your network password
@@ -16,6 +17,7 @@ const char server[] = "192.168.219.105";
 const char host[] = "192.168.219.105:8080";
 WiFiEspClient client; // WiFiEspClient 객체 선언
 
+//receive
 bool checkIrRecv1 = false; 
 bool checkIrRecv2 = false;
 int Recv_pin1 = 12;
@@ -27,6 +29,10 @@ IRrecv irrecv2(Recv_pin2); //IRrecv 객체 생성
 decode_results results2; // 수신 데이터 저장 구조체
 unsigned long currentTime = 0, previousTime = 0;
 String sensingData = "";
+
+//power
+int sw = 5;
+int power = 0;
 
 void setup() {
   Serial.begin(9600);  //시리얼모니터
@@ -44,11 +50,12 @@ void setup() {
   lcd.backlight();
   irrecv1.enableIRIn();
   irrecv2.enableIRIn();
+
+  pinMode(sw, INPUT_PULLUP);
 }
 
 void loop() {
-    if (!client.connected()) {   // 날씨정보 수신 종료됐으면
-      Serial.println();
+    if (!client.connected()) {  
       client.stop();
     }
 
@@ -62,18 +69,35 @@ void loop() {
         displayLCD(lcd, bat);
         sendBat();
     }
-
-    //센싱 데이터 전송
-    sensingData = sensing();
-    if(sensingData){
-      if(sensingData == "out"){
-        Serial.println("Out");
-        sendData("Out",2);
+    
+    //power switch
+    if(digitalRead(sw) == LOW){
+      delay(1000);
+      if(power == 0){ 
+        power += 1;
+        Serial.println("power on");
+        sendData("", "On", 3 );
       }
-      if(sensingData == "in"){
-        Serial.println("In");
-        sendData("In",2);
-      }  
+      else{
+        power -= 1;
+        Serial.println("power off");
+        sendData("","Off", 3);
+      }
+    }
+
+    //power가 on일때 센싱 데이터 전송
+    if(power == 1){
+    sensingData = sensing();
+      if(sensingData){
+        if(sensingData == "out"){
+          Serial.println("Out");
+          sendData("Out", "On", 2);
+        }
+        if(sensingData == "in"){
+          Serial.println("In");
+          sendData("In", "On", 2);
+        }  
+      }
     }
 }
 
@@ -84,9 +108,10 @@ String checkVoltage()
     input_vol = analogRead(A0);
     temp = input_vol/4.092;
     cal_vol = temp / 10;
-    cal_vol = cal_vol *  0.63613 * 0.901;  // 오차율 조절해야 함
-    cal_vol = (cal_vol / BAT) * 100;
-    cal_vol = (int)cal_vol;
+    cal_vol = cal_vol * 0.63613*0.9;  // 오차율 조절해야 함 (0.63613)
+    cal_vol = (int)round((cal_vol / BAT) * 100);
+    temp = (int)cal_vol % 10;
+    cal_vol -= temp;
     res = String(cal_vol);
     return res;
 }
@@ -158,7 +183,7 @@ String sensing(){
     return result;
 }
 
-void sendData(String result, int number){
+void sendData(String result, String power, int number){
   switch(number){
     case 1:
       url = "/update/rxbattery/?batteryCapacity=" + result + "&" + "deviceId=" + "123";
@@ -172,13 +197,23 @@ void sendData(String result, int number){
       break;
       
     case 2:
-      url = "/update/sensing/?state=" + result + "&" + "deviceId=" + "123" + "&" + "power=" + "On" + "&" + "userPk=" + "1234";
+      url = "/update/sensing/?state=" + result + "&" + "deviceId=" + "123" + "&" + "power=" + power;
+      Serial.println("Starting connection to server...");
+      if (client.connect(server, 8080)) {
+        Serial.println("Connected to server");
+        client.print("GET " + url + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "Connection: close\r\n\r\n");
+      }
+      break;
+      
+    case 3:
+      url = "/update/sensing/?state=" + result + "&" + "deviceId=" + "123" + "&" + "power=" + power;
       Serial.println("Starting connection to server...");
       if (client.connect(server, 8080)) {
         Serial.println("Connected to server");
         client.print("GET " + url + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "Connection: close\r\n\r\n");
       }
       break; 
+      
      default:
         Serial.println("Data select error");
   }
@@ -191,51 +226,43 @@ void sendBat()
     Serial.println();
     if (bat.toInt() == 100 && bat && bat < tempForBat)
     {
-      sendData("100",1);
-      Serial.print("bat: ");
-      Serial.println(bat);
+      sendData("100", "", 1);
     }
     else if (bat.toInt() == 90 && bat< tempForBat)
     {
-      sendData("90",1);
-      Serial.println("bat: ");
-      Serial.println(bat);
+      sendData("90","",1);
     }
     else if (bat.toInt() == 80 && bat< tempForBat)
     {
-      sendData("80",1);
-      Serial.println("bat: ");
-      Serial.println(bat);
+      sendData("80","",1);
     }
     else if (bat.toInt() == 70 && bat< tempForBat)
     {
-      sendData("70",1);
-      Serial.println("bat: ");
-      Serial.println(bat);
+      sendData("70","",1);
     }
     else if (bat.toInt() == 60 && bat< tempForBat)
     {
-      sendData("60",1);
+      sendData("60","",1);
     }
     else if (bat.toInt() == 50 && bat< tempForBat)
     {
-      sendData("50",1);
+      sendData("50","",1);
     }
     else if (bat.toInt() == 40 && bat< tempForBat)
     {
-      sendData("40",1);
+      sendData("40","",1);
     }
     else if (bat.toInt() == 30 && bat< tempForBat)
     {
-     sendData("30",1);
+     sendData("30","",1);
     }
     else if (bat.toInt() == 20 && bat< tempForBat)
     {
-      sendData("20",1);
+      sendData("20","",1);
     }   
     else if (bat.toInt() == 10 && bat< tempForBat)
     {
-      sendData("10",1);
+      sendData("10","",1);
     }   
 
     if (bat.toInt() < tempForBat.toInt()) //&& bat < tempForBat
